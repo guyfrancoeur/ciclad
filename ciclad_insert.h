9e60f0a9acb0427bc1e4dd0c5decd3f9a6c5dda5
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2019-10-04 12:35:15 -0400

    Create ciclad_insert.h

---
+++ ciclad_insert.h
@@ -0,0 +1,18 @@
+#ifndef _CICLAD_INSERT_H
+#define _CICLAD_INSERT_H
+
+#include <cstdio>   //fopen, printf
+#include <cstdlib>  //atol
+#include <queue>    //sliding window
+#include <cstring>  //strtok
+#include <ctime>    //clock_t
+#include <iostream> //cout
+
+#include "isci.h"   //unit ushort node3 concept3
+
+using namespace std;
+
+double add(char *s, auto tn, auto idx, node3 _rootchild, concept3 superconcept, auto fCI2, uint gCid);
+
+#endif
+
