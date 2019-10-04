15f05c70cb7a2fc9b67d9e52a6313353ac8b5d1d
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2019-10-04 12:05:48 -0400

    version 3.x compatible

--- isci.h
+++ isci.h
@@ -53,6 +53,31 @@ struct concept2 {
   ushort size;
   std::vector<uint> itemset;
 };
+//------------------------------------------------------------------------------
+//Version 3.x (a la C)
+struct node3 {
+  node3() : depth(0), key(0), nb_ref(0), Cid(0), parent(NULL) { }
+  node3(stx::btree_map<uint, node *> _enfant) : enfant(_enfant), depth(0), key(0), nb_ref(0), Cid(0), parent(NULL) { }
+
+  uint depth;
+  uint key;  //item
+  uint nb_ref;
+  uint Cid;  //Conecpt id
+  node *parent;
+  stx::btree_map<uint, node *> enfant;
+  std::vector<uint> gen;
+};
+
+struct concept3 {
+  concept3() : id(0), supp(0), size(0), deleted(0){}
+  concept3(uint _id, uint _supp, ushort _size, ushort _deleted) : id(_id), supp(_supp), size(_size), deleted(_deleted){}
+  concept3(uint _id, uint _supp, ushort _size, ushort _deleted, std::vector<uint> _itemset) : id(_id), supp(_supp), size(_size), deleted(_deleted), itemset(_itemset) {}
+  uint id;
+  uint supp;
+  ushort size;
+  ushort deleted; //flag 0 | 1
+  std::vector<uint> itemset;
+};
 
 //------------------------------------------------------------------------------
 //version 3.x (a la C++)
@@ -113,6 +138,7 @@ NodeR<T>::~NodeR() { /* free() or delete[] */ }
 
 //------------------------------------------------------------------------------
 template <class T> void wf(T p, string s);
-extern void freeNode(node *n); // v2.1h
+extern void freeNode(node *n);   // v2.1h
+extern void freeNode3(node3 *n); //v3.0
 
 #endif // _ISCI_H_
